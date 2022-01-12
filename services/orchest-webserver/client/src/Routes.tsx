e1623f2256ce351a6bceb8142bc128cec5865243
Huang-Ming Chang <huang@orchest.io>
2022-01-12 11:23:41 +0100

    move a side effect from Routes to HeaderBar

--- services/orchest-webserver/client/src/Routes.tsx
+++ services/orchest-webserver/client/src/Routes.tsx
@@ -1,29 +1,8 @@
-import { useProjectsContext } from "@/contexts/ProjectsContext";
 import React from "react";
-import { Redirect, Route, Switch, useLocation } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import { getOrderedRoutes, siteMap, toQueryString } from "./routingConfig";
 
 const Routes = () => {
-  let location = useLocation();
-  const { dispatch } = useProjectsContext();
-
-  React.useEffect(() => {
-    /*
-      Always unset the pipeline for the header bar on navigation. 
-      It's up to pages to request the headerbar pipeline if they 
-      need it.
-
-      TODO: move to HeaderBar in the future.
-    */
-    dispatch({
-      type: "pipelineSet",
-      payload: {
-        pipelineUuid: undefined,
-        pipelineName: undefined,
-      },
-    });
-  }, [location]);
-
   return (
     <Switch>
       <Route exact path="/">
